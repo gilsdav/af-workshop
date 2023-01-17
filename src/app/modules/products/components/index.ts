601a65f772b96cce3beb5737a278e92e9b8c8d38
David Gilson <d.gilson@afelio.be>
2023-01-17 09:42:12 +0100

    fix index

--- src/app/modules/products/components/index.ts
+++ src/app/modules/products/components/index.ts
@@ -1,7 +1,10 @@
 import { PizzaItemComponent } from './pizza-item/pizza-item.component';
 import { PizzaDisplayComponent } from './pizza-display/pizza-display.component';
 
-export const components: any[] = [PizzaItemComponent, PizzaDisplayComponent];
+export const components = [
+    PizzaItemComponent,
+    PizzaDisplayComponent
+];
 
 export * from './pizza-item/pizza-item.component';
 export * from './pizza-display/pizza-display.component';
