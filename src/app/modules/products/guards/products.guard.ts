b38e4c416b86854f1de82253911dbb3aedc5ae27
Axel Herman <a.herman@afelio.be>
2025-07-29 08:45:34 +0200

    Update to NG16

--- src/app/modules/products/guards/products.guard.ts
+++ src/app/modules/products/guards/products.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { AccessService } from '../services';
 
 @Injectable()
-export class ProductsGuard implements CanActivate {
+export class ProductsGuard  {
 
     constructor(
         private accessService: AccessService,
